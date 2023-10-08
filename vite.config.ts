74f9b9e9ad2ff1dadf697302b6e36b157761593a
Kristian Dupont <kristian@kristiandupont.com>
2023-10-08 15:45:20 +0000

    GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [], esbuild: { jsxDev: false} });
+export default defineConfig({
+  plugins: [],
+  esbuild: { jsxDev: false },
+  build: { outDir: "docs" },
+});
